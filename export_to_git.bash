f4ea4b0691b4c92a2619ad104610406f304cc97c
zuletav <andres.zv98@gmail.com>
2023-03-23 16:18:46 +0100

    stage deleted files

--- export_to_git.bash
+++
@@ -1,7 +0,0 @@
-#export to GIT:
-mkdir ~/common/python/gitpacks/
-rsync -va ~/common/python/include/ConeRot/*  ~/common/python/gitpacks/ConeRot/
-rsync -va ~/common/python/include/Cube2Im.py  ~/common/python/gitpacks/ConeRot/
-rsync -va ~/common/python/include/Resamp.py  ~/common/python/gitpacks/ConeRot/
-rsync -va ~/common/python/include/Vtools.py  ~/common/python/gitpacks/ConeRot/
-
